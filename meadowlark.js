fe3c07bb7aaf2b554a410041729ea8ad19e32f5b
Ananth Kumar Kambhampati <B026289@wppmbp201605142-wifi.ga.kp.org>
2019-10-23 17:52:37 -0400

    Moved 'fortune cookie' into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-
+const fortune = require('./lib/fortune')
 const app = express()
 
 // configure handlebars view engine
@@ -14,19 +14,11 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-]
 
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: randomFortune})
+  res.render('about', { fortune: fortune.getFortune()})
 })
 
 
